e9a3feb73bf7ab0bfb2b13ba738dde327f4d69b2
cristian_moreno <cristian.moreno@juju.com.co>
2022-06-13 19:39:29 -0500

    no message

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+
+describe("it should say hello", () => {
+  it("should greet 'Sergio'", () => {
+    //expect(hello()).toBe("Hello, Sergio");
+  });
+
+  it("should greet 'Daniel'", () => {
+    //expect(hello("Daniel")).toBe("Hello, Daniel");
+  });
+});
